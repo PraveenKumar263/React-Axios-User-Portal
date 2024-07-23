d61913f7b1de9856682bb3c01082c59f2b3d9770
PraveenKumar263 <PraveenKumar263>
2024-07-23 22:57:02 +0800

    setup route and component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { userProvider } from './components/UsersContext';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <userProvider>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/userData" component={UserList} /> 
+    </Switch>
+  </userProvider>
   );
 }
 
